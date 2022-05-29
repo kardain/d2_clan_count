4fe20c18f5d3c92f3b8735de1f8ea5d58eab3585
kardain <kardain@users.noreply.github.com>
2022-05-29 02:40:26 -0700

    moved strings from .py to env template

--- clancount.py
+++ clancount.py
@@ -3,113 +3,101 @@
 # 
 
 import time
-import datetime
-from datetime import date, datetime
 import requests
-from requests_toolbelt.utils import dump
 import os
-import sys
 import json
 from pathlib import Path
 from dotenv import load_dotenv
 
-# Workaround for ExceptionError (None variables from .env)
+#Workaround for None type when calling from .env
+
 script_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(script_dir)
 
-# Required modules
-import logging
+#required modules
+
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-# Header for API call, do not change.
-api_auth_headers = {
-    'X-API-Key': os.getenv('BUNGIE-API-KEY'),
-}
-
-# Required constants
-
+import logging
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.DEBUG,
+    level=logging.os.getenv('LOGGING_LEVEL'),
     datefmt='%Y-%m-%d %H:%M:%S')
 
-epoch_time = round(int(time.time()))
+# Header for API call, do not change.
+api_auth_headers = {'X-API-Key': os.getenv('BUNGIE-API-KEY')}
 
-# Do the things
+###
 
 def main():
-    logging.info('Welcome Guardian')
+    logging.info(os.getenv('WELCOME-TEXT'))
     
-    clan_URI = 'https://www.bungie.net/Platform/GroupV2/' + os.getenv('CLAN_ID') + '/'
+    clanURI = 'https://www.bungie.net/Platform/GroupV2/' + os.getenv('CLAN_ID')
     
-    logging.info(f'Connecting to Bungie API -- {clan_URI}')
+    logging.info(f"{os.getenv('CONNECTING')} -- {clanURI}")
     
-    clan_get = requests.get(clan_URI, headers=api_auth_headers)
+    clanget = requests.get(clanURI, headers=api_auth_headers)
     
-    logging.debug(clan_get)
+    logging.debug(clanget)
     
-    if clan_get.status_code != 200:
-        logging.error('There was a problem getting info from the API. Aborting')
+    if clanget.status_code != 200:
+        logging.error(os.getenv('ERROR'))
         exit()
     
-    logging.info('Fetching member count...')
+    logging.info(os.getenv('FETCHING'))
     
-    api_count = clan_get.json()['Response']['detail']['memberCount']
+    APIcount = clanget.json()['Response']['detail']['memberCount']
     
     with open ('clanget.json','r') as infile:
         current_json = json.load(infile)
         
     if current_json['first_run'] == "True":
-        logging.info('First run detected. Updating local file with current information.')
-        current_json.update({"memberCount": api_count})
+        logging.info(os.getenv('FIRST-RUN-TRUE'))
+        current_json.update({"memberCount": APIcount})
         current_json.update({"first_run": "False"})
         with open ('clanget.json','w') as outfile:
             json.dump(current_json,outfile,indent = 4)
         time.sleep(1)
-        logging.info('First run complete. Exiting.')
+        logging.info(os.getenv('FIRST-RUN-COMPLETE'))
         exit()
                         
     current_count = current_json['memberCount']    
-    logging.info(f'Current count: {current_count}')
-    logging.info(f'API reports: {api_count}')
+    logging.info(f"Current count: {current_count}")
+    logging.info(f"API reports: {APIcount}")
     
-    if current_count == api_count:
-        logging.info('Counts match. Exiting.')
+    if current_count == APIcount:
+        logging.info(os.getenv('COUNT-MATCHES'))
         exit()
     else:
-        logging.info('Found a change in counts.')
+        logging.info(os.getenv('COUNT-CHANGE'))
 
         if os.getenv('SEND_TO_DISCORD') == 'True':
             ##Build Discord Embed
-            discord_headers = {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-            }
-            
+            discord_headers = {'Content-Type': 'application/json','Accept': 'application/json'}            
             uri = os.getenv('DISCORD-WEBHOOK')
             payload = {
                 "username": os.getenv('DISCORD_USERNAME'),
                 "avatar_url": os.getenv('DISCORD_AVATAR'),
-                "content": "",
+                "content": "\u200c",
                 "embeds": [
                     {
                         "type": "rich",
-                        "title": "Member Count Change",
-                        "description": f"\nUpdate as of <t:{epoch_time}:R>\n"
+                        "title": os.getenv('DISCORD_TITLE'),
+                        "description": f"\n{os.getenv('DISCORD_DESC_1')} <t:{round(int(time.time()))}:R>\n"
                                        "\n"
-                                       f"Current member count is now {api_count}\n\n",
-                        "color": 0x1e1d0e
+                                       f"{os.getenv('DISCORD_DESC_2')} {APIcount}\n\n",
+                        "color": int(os.getenv('DISCORD_COLOR'))#0x1e1d0e
                     }
                 ]
             }
-            logging.debug(payload)
+            logging.info(payload)
             response = requests.post(uri, headers=discord_headers, json=payload)
-            logging.debug(response)
+            logging.info(response)
         
-        logging.info('Writing new count to file and exiting.')
+        logging.info(os.getenv('LOCAL-FILE-UPDATE'))
         
-        current_json.update({"memberCount": api_count})
+        current_json.update({"memberCount": APIcount})
             
         with open ('clanget.json','w') as outfile:
             json.dump(current_json,outfile,indent = 4)
@@ -118,3 +106,4 @@ def main():
  
 if __name__ == "__main__":
     main()
+
